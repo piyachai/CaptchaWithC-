67dc6e3c556fc29767078a4c7d4d2ce2458cd6a2
piyachai <kaunpramote.p@gmail.com>
2015-05-13 22:44:35 +0700

    Edit Project CaptchaTest

---
+++ CaptchaTest/ServiceCaptchaTest.cs
@@ -0,0 +1,16 @@
+﻿using CaptchaLibrary;
+using NUnit.Framework;
+
+namespace CaptchaTest
+{
+    [TestFixture]
+    public class ServiceCaptchaTest
+    {
+        [Test]
+        public void GetCaptchaObject()
+        {
+            ServiceCaptcha serviceCaptcha = new ServiceCaptcha();
+            Assert.AreEqual("ONE+1", serviceCaptcha.getCaptcha());
+        }
+    }
+}
